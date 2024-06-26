c9785b9c119d45e9189d3322218febc087cf5262
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-26 16:35:55 +0100

    feat: FIFOcache class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+""" FIFOCache class """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCAche definition"""
+    def __init__(self):
+        """initialization method"""
+        super().__init__()
+        self.cache_list = []
+
+    def put(self, key, item):
+        """add item to cache"""
+        if key is None or item is None:
+            return
+        elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = self.cache_list.pop(0)
+            print(f"DISCARD {discarded_key}")
+        self.cache_list.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ return item from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
