2715514f6429680cee1017dbf269a8417f15bea2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-26 13:54:48 +0100

    feat: create BasicCache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" BasicCache class """
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+    def __init__(self):
+        """initialization method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ add item to cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
