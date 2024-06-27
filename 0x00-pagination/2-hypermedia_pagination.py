a6f51625cea141292ec11c9b46aefcb954412e7c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-27 13:49:49 +0100

    fix: change  logic for next and prev page dict entries

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -41,14 +41,14 @@ class Server:
         return page_content
 
     def get_hyper(self, page: int = 1,
-                  page_size: int = 10) -> Dict[str, Union[int, List[List]]]:
+                  page_size: int = 10) -> Dict[str, Union[int, List[List], None]]:
         """ hypermedia metadata generator"""
-        total_pages = math.ceil((len(self.dataset()) + 1) / page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
             "page_size": page_size,
             "page": page,
             "data": self.get_page(page, page_size),
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1,
+            "next_page": page + 1 if page <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
         }
