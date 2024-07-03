5b6c87c444bae1fb10f0cc520d7c5a2b630bc960
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-03 17:15:04 +0100

    feat: setup Flask-Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+""" flask app"""
+from flask import flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('Config')
