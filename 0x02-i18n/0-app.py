0759709a9a367d2920784e8be634b4357ec82301
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-03 16:12:39 +0100

    feat: simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" flask app"""
+from flask import flask
+# from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ route for index page of flask app """
+    return render_template('0-index.html')
