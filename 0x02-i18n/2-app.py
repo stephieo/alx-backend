29c6cb624ed91567a37eb43ca2f3aa77a433d640
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-04 14:02:50 +0100

    feat: get_locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+""" flask app with Babel setup"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+def get_locale():
+    """ function that determines the best language setting
+    for the current HTTP request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+class Config():
+    """babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('1-app.Config')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ route for index page of flask app """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
