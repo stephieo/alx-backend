1008b7cac97f469732782b777ed3130c55141fc5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-04 12:06:57 +0100

    fix: include route

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" flask app"""
+""" flask app with Babel setup"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -15,6 +15,13 @@ class Config():
 
 
 app.config.from_object('Config')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ route for index page of flask app """
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
