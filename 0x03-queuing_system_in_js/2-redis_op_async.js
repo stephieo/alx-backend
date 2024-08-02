276487aa629ba33b2d4f6422a9849ef02ef29b56
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-02 20:58:35 +0100

    feat: promisified Redis get method

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+//  redis package exports using CommonJS.
+//  Import the whole module and then destructure it:
+import * as redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server', err);
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, redis.print);
+}
+
+const asyncRedisGet = promisify(client.GET).bind(client);
+async function displaySchoolValue(schoolName) {
+  const value = await asyncRedisGet(schoolName);
+  console.log(value)
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco','100');
+displaySchoolValue('HolbertonSanFrancisco');
