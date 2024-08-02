caf9df94672cba659fd69fb0d6b03493c6f53d6c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-02 20:33:46 +0100

    feat: node_redis functions to interact with Redis server

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+//  redis package exports using CommonJS.
+//  Import the whole module and then destructure it:
+import * as redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server', err);
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.GET(schoolName, redis.print);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco','100');
+displaySchoolValue('HolbertonSanFrancisco');
