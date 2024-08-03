9e49c3a06e038a8f4fe72940619a473e0fb9f0a8
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 01:10:02 +0100

    feat: pub/sub setup

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+import * as redis from 'redis';
+
+const subscriber = redis.createClient();
+
+subscriber.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+subscriber.on('error', (err) => {
+  console.log('Redis client not connected to the server: ', err);
+});
+
+// subcribe to channel
+subscriber.subscribe('holberton school channel');
+
+//  listen for incoming messages
+subscriber.on('message', (channel, msg) => {
+  if (msg === 'KILL_SERVER') {
+    subscriber.unsubscribe(`${channel}`);
+    subscriber.quit();
+  }
+  console.log(`${msg}`);
+});
