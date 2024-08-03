ee8f6d19e225fd7c0bffb97789595c3aecaea3bf
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 02:10:13 +0100

    feat: job creator function

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+import * as kue from 'kue';
+
+// object with Job data
+
+const jobData = {
+  phoneNumber: '09087654321',
+  message: 'this is a job',
+};
+
+// create queue
+const push_notification_code = kue.createQueue();
+
+// create Job
+
+const job = push_notification_code.create('notification', jobData)
+  .save((err) => {
+    if (err) {
+      console.log('Notification job creation failed: ', err);
+    } else {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// job event handler
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
