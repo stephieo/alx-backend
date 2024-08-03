97c7c75659354314613fbdb30d584eab73ffd462
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 03:54:37 +0100

    test: testing job creation function

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,1 +1,71 @@
 #!/usr/bin/node
+import createPushNotificationsJobs from './8-job';
+import kue from 'kue';
+import { expect } from 'chai';
+
+const testQueue = kue.createQueue();
+
+const jobs = [
+    {
+	phoneNumber: '+24153518780',
+	message: 'This is the code 1234 to verify your account',
+    },
+    {
+	phoneNumber: '+25153118782',
+	message: 'This is the code 38998 to verify your account',
+    },
+];
+
+describe('createPushNotificationsJobs', () => {
+    before(() => {
+	testQueue.testMode.enter();
+    });
+
+    afterEach(() => {
+	testQueue.testMode.clear();
+    });
+
+    after(() => {
+	testQueue.testMode.exit();
+    });
+
+    it('throw a new error if jobs is not an array', () => {
+	expect(() => {
+	    createPushNotificationsJobs(2, testQueue);
+	}).to.throw('Jobs is not an array');
+    });
+
+    it('throw a new error if jobs is not an array', () => {
+	expect(() => {
+	    createPushNotificationsJobs("wqfew", testQueue);
+	}).to.throw('Jobs is not an array');
+    });
+
+    it('throw a new error if jobs is not an array', () => {
+	expect(() => {
+	    createPushNotificationsJobs({"Name": "ALX"}, testQueue);
+	}).to.throw('Jobs is not an array');
+    });
+
+    it('Not throw any errors, return undefined', () => {
+	const retValue = createPushNotificationsJobs([], testQueue);
+	expect(retValue).to.equal(undefined);
+    });
+
+    it('Adds new jobs to the testQueue through the jobs array', () => {
+	createPushNotificationsJobs(jobs, testQueue);
+	expect(testQueue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+
+	expect(testQueue.testMode.jobs[0].data).to.eql({
+	    phoneNumber: '+24153518780',
+	    message: 'This is the code 1234 to verify your account',
+	});
+
+	expect(testQueue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+
+	expect(queue.testMode.jobs[1].data).to.eql({
+	    phoneNumber: '+25153118782',
+	    message: 'This is the code 38998 to verify your account',
+	});
+    });
+});
