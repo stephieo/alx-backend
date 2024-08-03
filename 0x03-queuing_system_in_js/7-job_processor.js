0740e3f56d047c05fe505c5038090db546bc58cb
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 03:38:59 +0100

    feat: track progress and errors in job processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+import * as kue from 'kue';
+
+const queue = kue.createQueue();
+
+const blacklist = ['4153518780', '4153518780'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blacklist.includes(phoneNumber)) {
+   done(Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber} with message: ${message}`);
+    done();
+  }
+}
+
+queue.process('notification', 2, (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+})
