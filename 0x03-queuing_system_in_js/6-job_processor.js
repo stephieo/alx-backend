eedeaad53e665b97fcd60a48eedbe05bf46f306b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 02:31:35 +0100

    feat:job processor function

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+import * as kue from 'kue';
+
+// create queue
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber} with message: ${message}`);
+}
+
+queue.process('notification', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
