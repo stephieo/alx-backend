805bded326551ca09de4596bc1a6a0fdff149213
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 03:18:43 +0100

    feat: job creator with progress tracking

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -1,1 +1,67 @@
 #!/usr/bin/node
+import * as kue from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account',
+  },
+];
+
+const push_notification_code_2 = kue.createQueue();
+
+jobs.forEach((item) => {
+  const job = push_notification_code_2.create('notification', item)
+    .save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  }).on('failed', (err) => {
+    console.log(`Notification job ${job.id} failed: `, err);
+  }).on('progress', (progress, data) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
